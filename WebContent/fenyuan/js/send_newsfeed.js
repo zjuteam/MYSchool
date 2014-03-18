47ddfbf87f9420f11dfb5d2e07aa081ec13a00a3
Benson <tounaobun@gmail.com>
2014-03-18 21:24:28 +0800

    newsfeed dialog added

--- WebContent/fenyuan/js/send_newsfeed.js
+++ WebContent/fenyuan/js/send_newsfeed.js
@@ -1,3 +1,23 @@
 $(function() {
 	changeImgWhenHover();
+	publishNewsfeed();
+	close();
 });
+
+function publishNewsfeed() {
+	$('#create_acti').click(function() {
+		$('#dialog-form').dialog({
+			      height: 472,
+			      width: 640,
+			      draggable: false,
+			      resizable: false,
+			      modal: true	/* prevent user from interacting with the rest of the page. */
+		});
+	});
+}
+
+function close() {
+	$('input[type="button"]').click(function() {
+		$('#dialog-form').dialog("close");
+	});
+}
