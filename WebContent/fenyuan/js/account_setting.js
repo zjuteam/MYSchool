c0adfc60cc8def971d9d616f50d7388c24161866
Benson <tounaobun@gmail.com>
2014-03-18 18:50:33 +0800

    account setting

--- WebContent/fenyuan/js/account_setting.js
+++ WebContent/fenyuan/js/account_setting.js
@@ -1,3 +1,21 @@
 $(function() {
 	changeImgWhenHover();
+	edit();
+	cancel();
 });
+
+function edit() {
+	$('#edit').children().click(function() {
+		$('#container').hide();
+		$('#edit_container').show();		
+	});
+}
+
+function cancel() {
+	$('input[type="button"]').click(function() {
+		// show dialog
+		$(this).dialog();
+		//$('#container').show();
+		//$('#edit_container').hide();		
+	});
+}
--- WebContent/fenyuan/js/org_profile.js
+++ WebContent/fenyuan/js/org_profile.js
@@ -1,3 +1,25 @@
 $(function() {
 	changeImgWhenHover();
+	clearHint();
 });
+
+function clearHint() {
+	$('input[type="text"]').focus(function() {
+		if($(this).val() == '' || $(this).val() == '标题')
+			$(this).val('');
+	});
+	$('input[type="text"]').blur(function() {
+		if($(this).val() == '') {
+			$(this).val('标题');
+		}
+	});
+	$('textarea').focus(function() {
+		if($(this).val() == '正文' || $(this).val() == '')
+		$(this).val('');
+	});
+	$('textarea').blur(function() {
+		if($(this).val() == '') {
+			$(this).val('正文');
+		}
+	});	
+}
