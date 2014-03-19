86ad7c42fa608699a5dfcf6b648bae4f9654a4df
Benson <tounaobun@gmail.com>
2014-03-19 17:18:32 +0800

    add

--- WebContent/fenyuan/js/send_activities.js
+++ WebContent/fenyuan/js/send_activities.js
@@ -1,3 +1,28 @@
 $(function() {
 	changeImgWhenHover();
+	publishActivities();
+	close();
+	pickTime();
 });
+
+function publishActivities() {
+	$('#create_acti').click(function() {
+		$('#dialog-form').dialog({
+			      height: 610,
+			      width: 646,
+			      draggable: false,
+			      resizable: false,
+			      modal: true	/* prevent user from interacting with the rest of the page. */
+		});
+	});
+}
+
+function pickTime() {
+	$('#start_time').datepicker();
+	$('#end_time').datepicker();
+}
+function close() {
+	$('input[type="button"]').click(function() {
+		$('#dialog-form').dialog("close");
+	});
+}
--- WebContent/fenyuan/js/send_newsfeed.js
+++ WebContent/fenyuan/js/send_newsfeed.js
@@ -3,6 +3,7 @@ $(function() {
 	publishNewsfeed();
 	close();
 	testAddNewsfeed();
+	confirmDelete();
 });
 
 function publishNewsfeed() {
@@ -36,5 +37,18 @@ function testAddNewsfeed() {
 				+ "		<div class='edit'><table><tr><td rowspan='2'><img src='img/u211_line.png' /></td><td><img src='img/u212.png' /></td><td><img src='img/u214.png' /></td></tr><tr><td>编辑</td><td>删除</td></tr></table></div></div>");
 		
 	   $("#dialog-form").dialog( "close" );	// close dialog
+	});
+}
+
+function confirmDelete() {
+	$('#confirm_delete').click(function() {
+		$('#dialog-confirm').dialog({
+			 height: 223,
+		      width: 403,
+		      draggable: false,
+		      resizable: false,
+		      modal: true
+		});
+		$('#dialog-confirm').css('border-radius','6px');
 	});
 }
