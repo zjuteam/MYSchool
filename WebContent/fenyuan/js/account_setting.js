0b4a10ba09da4722a60a7c57d78985395cc7cd72
Benson <tounaobun@gmail.com>
2014-03-31 01:26:25 +0800

    取消对话框

--- WebContent/fenyuan/js/account_setting.js
+++ WebContent/fenyuan/js/account_setting.js
@@ -1,8 +1,9 @@
 $(function() {
-	edit();
 	cancel();
 	changePortrait();
+	dialogEvent();
 });
+
 
 function changePortrait() {
 	$("#change_portrait").change(function(){
@@ -15,18 +16,32 @@ function changePortrait() {
 	    }
 	});
 }
-function edit() {
-	$('#edit').children().click(function() {
-		$('#container').hide();
-		$('#edit_container').show();		
+
+/* 帐号设置编辑保存 */
+function save() {
+	$('input[type="submit"]').click(function() {
+		// 保存
 	});
 }
-
+/* 帐号设置编辑取消 */
 function cancel() {
 	$('input[type="button"]').click(function() {
-		// show dialog
-		$(this).dialog();
-		//$('#container').show();
-		//$('#edit_container').hide();		
+		
+		$('#dialog-form').dialog({
+		      height: 220,
+		      width: 400,
+		      draggable: false,
+		      resizable: false,
+		      modal: true	/* prevent user from interacting with the rest of the page. */
+	});
+	});
+}
+/* 对话框是否事件 */
+function dialogEvent() {
+	$('.yes').click(function() {
+		window.location.href="account_setting.html";
+	});
+	$('.no').click(function() {
+		$('#dialog-form').dialog("close");
 	});
 }
--- WebContent/lib/jquery-menu.js
+++
@@ -1,7 +0,0 @@
-/*
- * second-level menu plugin
- * @author:Benson
- */
-$(function($) {
-	alert(0);
-}(jQuery));
