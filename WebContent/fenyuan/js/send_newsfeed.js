21add28a9cb0789f2f5711f16f171e90097cfa52
Benson <tounaobun@gmail.com>
2014-04-02 16:24:13 +0800

    some modifications

--- WebContent/fenyuan/js/send_newsfeed.js
+++ WebContent/fenyuan/js/send_newsfeed.js
@@ -45,14 +45,42 @@ function close() {
 function testAddNewsfeed() {
 	$('#dialog-form input[type="submit"]').click(function(event) {
 		event.preventDefault();
-		var theme = $("#theme");
-		var tag = $("#tag");
+		var theme = $("#theme").val();
+		var tag = $("#tag").val();
 		
-		$('#down').append("<div class='newsfeed'><div class='info'><table><tr class='title'>"
-				+ "<td rowspan='3'><img src='img/u271.png' /></td><td>标题:</td><td>" + theme.val()
-				+ "</td></tr><tr class='last_edit'><td>上次编辑：</td><td>2013-11-12-11: 12</td></tr>"
-				+ "<tr class='tag'><td>标签：</td><td>" + tag.val() + "</td></tr></table></div>"
-				+ "		<div class='edit'><table><tr><td rowspan='2'><img src='img/u211_line.png' /></td><td><img src='img/u212.png' /></td></tr><tr><td>编辑</td></tr></table></div></div>");
+		$('#down').append('<div class="newsfeed">'
+				+ '<div class="check">'
+				+ '<input type="checkbox" />'
+				+ '</div>'
+				+ '<div class="info">'
+				+ '<table>'
+				+ '<tr  class="title">'
+				+ '<td rowspan="3"><img src="img/u271.png" /></td>'
+				+ '<td>标题:</td>'
+				+ '<td>' + theme + '</td>'
+				+ '</tr>'	
+				+ '<tr class="last_edit">'
+				+ '<td>上次编辑：</td>'
+				+ '<td>2013-11-12-11：12</td>'
+				+ '</tr>'
+				+ '<tr class="tag">'
+				+ '<td>标签：</td>'
+				+ '<td>' + tag + '</td>'
+				+ '	</tr>'
+				+ '</table>'
+				+ '</div>'
+				+ '<div class="edit">'
+				+ '	<table>'
+				+ '<tr>'
+				+ '<td rowspan="2"><img src="img/u211_line.png" /></td>'
+				+ '<td><img src="img/u212.png" /></td>'
+				+ '</tr>'
+				+ '<tr>'
+				+ '<td>编辑</td>'
+				+ '</tr>'
+				+ '	</table>'
+				+ '</div>'
+				+ '</div>');
 		
 	   $("#dialog-form").dialog( "close" );	// close dialog
 	});
