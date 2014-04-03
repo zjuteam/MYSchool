2073ab6e5cfa082475b0da4eec366ab136550990
Benson <tounaobun@gmail.com>
2014-04-03 09:23:15 +0800

    项目结构重构

--- WebContent/js/fenyuan/send_newsfeed.js
+++ WebContent/js/fenyuan/send_newsfeed.js
@@ -1,3 +1,104 @@
+$(function() {
+	publishNewsfeed();
+});
+
+
+function clearPhoneHint() {
+	$('#input_phone').focus(function() {
+		if($(this).val() == '输入手机号码')
+			$(this).val('');
+	});
+	
+	$('#input_phone').blur(function() {
+		if($(this).val() == '') {
+			$(this).val('输入手机号码');
+		}
+	});
+	
+}
+function publishNewsfeed() {
+	$('#create_news').click(function() {
+		$('#dialog-form').dialog({
+				 open: function(event, ui) {
+				   $(this).load('dialog/create_newsfeed_dialog.html', function() {
+				   });
+				  },
+				  close: function(event, ui) {
+				  },	
+				  show: { effect: "fold", duration: 300
+				  },
+			      height: 502,
+			      width: 640,
+			      draggable: false,
+			      resizable: false,
+			      modal: true	/* prevent user from interacting with the rest of the page. */
+		});
+	});
+}
+
+function close() {
+	$('input[type="button"]').click(function() {
+		$('#dialog-form').dialog("close");
+	});
+}
+
+function testAddNewsfeed() {
+	$('#dialog-form input[type="submit"]').click(function(event) {
+		event.preventDefault();
+		var theme = $("#theme").val();
+		var tag = $("#tag").val();
+		
+		$('#down').append('<div class="newsfeed">'
+				+ '<div class="check">'
+				+ '<input type="checkbox" />'
+				+ '</div>'
+				+ '<div class="info">'
+				+ '<table>'
+				+ '<tr  class="title">'
+				+ '<td rowspan="3"><img src="../../img/fenyuan/u271.png" /></td>'
+				+ '<td>标题:</td>'
+				+ '<td>' + theme + '</td>'
+				+ '</tr>'	
+				+ '<tr class="last_edit">'
+				+ '<td>上次编辑：</td>'
+				+ '<td>2013-11-12-11：12</td>'
+				+ '</tr>'
+				+ '<tr class="tag">'
+				+ '<td>标签：</td>'
+				+ '<td>' + tag + '</td>'
+				+ '	</tr>'
+				+ '</table>'
+				+ '</div>'
+				+ '<div class="edit">'
+				+ '	<table>'
+				+ '<tr>'
+				+ '<td rowspan="2"><img src="../../img/fenyuan/u211_line.png" /></td>'
+				+ '<td><img src="../../img/fenyuan/u212.png" /></td>'
+				+ '</tr>'
+				+ '<tr>'
+				+ '<td>编辑</td>'
+				+ '</tr>'
+				+ '	</table>'
+				+ '</div>'
+				+ '</div>');
+		
+	   $("#dialog-form").dialog( "close" );	// close dialog
+	});
+}
+
+function confirmDelete() {
+	$('#confirm_delete').click(function() {
+		$('#dialog-confirm').dialog({
+			 height: 223,
+		      width: 403,
+		      draggable: false,
+		      resizable: false,
+		      modal: true
+		});
+		$('#dialog-confirm').css('border-radius','6px');
+	});
+}
+
 var originPic;
 
 function openFilePickDialog() {
@@ -107,18 +208,3 @@ function pickImg() {
 	    });
 	});
 }
-	$(function() {
-		// hide snapshot2,3
-		$('#snapshot2').hide();
-		$('#snapshot3').hide();
-		changeImg();
-		save();
-		del();
-		close();
-		testAddNewsfeed();
-		confirmDelete();
-		clearPhoneHint();
-		openFilePickDialog();
-		pickImg();
-		validate();
-	});
